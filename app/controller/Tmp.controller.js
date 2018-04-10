55971ebfe1ca2ebdbe511c157c7b8582bade2dde
unknown <battilana.matteo@gmail.com>
2018-04-10 15:50:06 +0200

    il treetable adesso funziona bene con il collassamento automatico ben implementato per ogni casistica

--- app/controller/Tmp.controller.js
+++ app/controller/Tmp.controller.js
@@ -8,189 +8,68 @@ sap.ui.define([
     "use strict";
     var TmpController = Controller.extend("myapp.controller.Tmp", {
 
-//        TreeNames: { 
-//            TreeTable_PresaInCarico:
-//                    {
-//                name:"TreeTable_PresaInCarico",
-//                FlagCollapseNotRelevant:1
-//            },
-//        TreeTable_FinePredisposizione1: {name:"TreeTable_FinePredisposizione1", FlagCollapseNotRelevant:1},
-//        { TreeTable_FinePredisposizione2: {name:"TreeTable_FinePredisposizione2", FlagCollapseNotRelevant:1}
-
-//        }
-
-        FlagCollapseNotRelevant_PresaInCarico: 1,
-
-        oModel_PresaInCarico: null,
-        View_PresaInCarico: null,
-        total_PresaInCarico: null,
-        FlagPresaInCarico: null,
-        AllExpanded_PresaInCarico: 0,
-
-        FlagFinePredisposizione1: null,
-        oModel_FinePredisposizione1: null,
-        View_FinePredisposizione1: null,
-        total_FinePredisposizione1: null,
-        AllExpanded_FinePredisposizione1: 0,
-        FlagFinePredisposizione2: null,
-        oModel_FinePredisposizione2: null,
-        View_FinePredisposizione2: null,
-        total_FinePredisposizione2: null,
-        AllExpanded_FinePredisposizione2: 0,
+        oModel: null,
+        View: null,
+        total: null,
+        oGlobalBusyDialog: new sap.m.BusyDialog(),
 
         onInit: function () {
             this.getSplitAppObj().toDetail(this.createId("Home"));
-            this.FlagPresaInCarico = null;
         },
-
         PresaInCarico: function () {
-            if (this.FlagPresaInCarico == null) {
+            this.getSplitAppObj().toDetail(this.createId("PresaInCarico"));
+            this.FillTreeTable("model/SKU.json", "TreeTable_PresaInCarico");
+        },
+        CollapseNotRelevant: function (TreeName) {
 
-                this.getSplitAppObj().toDetail(this.createId("PresaInCarico"));
-                this.oModel_PresaInCarico = new JSONModel("model/SKU.json");
-                this.getView().setModel(this.oModel_PresaInCarico);
-                this.View_PresaInCarico = this.getView().byId("TreeTable_PresaInCarico");
-                this.View_PresaInCarico.expandToLevel(100);
-                this.FlagPresaInCarico = 0;
-                this.FlagCollapseNotRelevant_PresaInCarico = 1;
-                this.FlagFinePredisposizione1 = null;
-                this.FlagFinePredisposizione2 = null;
-            }
-        },
-        CollapseNotRelevant: function () {
-            if (this.FlagCollapseNotRelevant_PresaInCarico == 1 && this.View_PresaInCarico !== null) {
-                var oGlobalBusyDialog = new sap.m.BusyDialog();
-                oGlobalBusyDialog.open();
-                this.View_PresaInCarico = this.getView().byId("TreeTable_PresaInCarico");
-                this.total = this.View_PresaInCarico._iBindingLength;
-                var temp;
-                for (var i = this.total - 1; i >= 0; i--) {
-                    temp = this.View_PresaInCarico.getContextByIndex(i).getObject();
-                    if (temp.expand == 0) {
-                        this.View_PresaInCarico.collapse(i);
-                    }
+            this.View = this.getView().byId(TreeName);
+            this.total = this.View._iBindingLength;
+            var temp;
+            for (var i = this.total - 1; i >= 0; i--) {
+                temp = this.View.getContextByIndex(i).getObject();
+                if (temp.expand == 0) {
+                    this.View.collapse(i);
                 }
-                oGlobalBusyDialog.close();
-                this.FlagCollapseNotRelevant_PresaInCarico = 0;
             }
+            this.oGlobalBusyDialog.close();
         },
-        CollapseAll_PresaInCarico: function () {
-            this.View_PresaInCarico = this.getView().byId("TreeTable_PresaInCarico");
-            this.View_PresaInCarico.collapseAll();
-            this.AllExpanded_PresaInCarico = 0;
-            var button = this.getView().byId("collapseButton");
-            button.setEnabled(false);
+        CollapseAll: function (event) {
+            var name = event.getSource().data("mydata");
+            this.View = this.getView().byId(name);
+            this.View.collapseAll();
         },
-        ExpandAll_PresaInCarico: function () {
-            this.View_PresaInCarico = this.getView().byId("TreeTable_PresaInCarico");
-            this.View_PresaInCarico.expandToLevel(100);
-            this.AllExpanded_PresaInCarico = 1;
-            var button = this.getView().byId("collapseButton");
-            button.setEnabled(true);
+        ExpandAll: function (event) {
+            var name = event.getSource().data("mydata");
+            this.View = this.getView().byId(name);
+            this.View.expandToLevel(100);
         },
-        ShowRelevant_PresaInCarico: function () {
-            this.FlagCollapseNotRelevant_PresaInCarico = 1;
-            this.CollapseNotRelevant_PresaInCarico();
-            var button = this.getView().byId("collapseButton");
-            button.setEnabled(false);
+        ShowRelevant: function (event) {
+            var name = event.getSource().data("mydata");
+            this.View = this.getView().byId(name);
+            this.View.expandToLevel(100);
+            setTimeout(jQuery.proxy(this.CollapseNotRelevant, this, name), 400);
         },
         FinePredisposizione: function () {
             this.getSplitAppObj().toDetail(this.createId("FinePredisposizione"));
-            this.FlagPresaInCarico = null;
-            if (this.FlagFinePredisposizione1 == null) {
 
-                this.oModel_FinePredisposizione1 = new JSONModel("model/SKU.json");
-                this.getView().setModel(this.oModel_FinePredisposizione1);
-                this.View_FinePredisposizione1 = this.getView().byId("TreeTable_FinePredisposizione1");
-                this.View_FinePredisposizione1.expandToLevel(100);
-                this.FlagFinePredisposizione1 = 0;
-                this.FlagCollapseNotRelevant_FinePredisposizione1 = 1;
-            }
+            this.FillTreeTable("model/SKU.json", "TreeTable_FinePredisposizione1");
+            this.FillTreeTable("model/SKU.json", "TreeTable_FinePredisposizione2");
+        },
 
-            if (this.FlagFinePredisposizione2 == null) {
+        Expander: function (name) {
+            this.View = this.getView().byId(name);
+            this.View.expandToLevel(100);
+        },
+        FillTreeTable: function (model_path, TreeName) {
+            this.oModel = new JSONModel(model_path);
+            this.getView().setModel(this.oModel);
+            this.View = this.getView().byId(TreeName);
+            this.oGlobalBusyDialog.open();
 
-                this.oModel_FinePredisposizione2 = new JSONModel("model/SKU.json");
-                this.getView().setModel(this.oModel_FinePredisposizione2);
-                this.View_FinePredisposizione2 = this.getView().byId("TreeTable_FinePredisposizione2");
-                this.View_FinePredisposizione2.expandToLevel(100);
-                this.FlagFinePredisposizione2 = 0;
-                this.FlagCollapseNotRelevant_FinePredisposizione2 = 1;
-            }
+            setTimeout(jQuery.proxy(this.Expander, this, TreeName), 200);
+            setTimeout(jQuery.proxy(this.CollapseNotRelevant, this, TreeName), 400);
         },
-//        CollapseNotRelevant_FinePredisposizione1: function () {
-//            if (this.FlagCollapseNotRelevant_FinePredisposizione1 == 1 && this.View_FinePredisposizione1 !== null) {
-//                var oGlobalBusyDialog = new sap.m.BusyDialog();
-//                oGlobalBusyDialog.open();
-//                this.View_FinePredisposizione1 = this.getView().byId("TreeTable_FinePredisposizione1");
-//                this.total = this.View_FinePredisposizione1._iBindingLength;
-//                var temp;
-//                for (var i = this.total - 1; i >= 0; i--) {
-//                    temp = this.View_FinePredisposizione1.getContextByIndex(i).getObject();
-//                    if (temp.expand == 0) {
-//                        this.View_FinePredisposizione1.collapse(i);
-//                    }
-//                }
-//                oGlobalBusyDialog.close();
-//                this.FlagCollapseNotRelevant_FinePredisposizione1 = 0;
-//            }
-//        },
-        CollapseAll_FinePredisposizione1: function () {
-            this.View_FinePredisposizione1 = this.getView().byId("TreeTable_FinePredisposizione1");
-            this.View_FinePredisposizione1.collapseAll();
-            this.AllExpanded_FinePredisposizione1 = 0;
-            var button = this.getView().byId("collapseButton_FinePredisposizione1");
-            button.setEnabled(false);
-        },
-        ExpandAll_FinePredisposizione1: function () {
-            this.View_FinePredisposizione1 = this.getView().byId("TreeTable_FinePredisposizione1");
-            this.View_FinePredisposizione1.expandToLevel(100);
-            this.AllExpanded_FinePredisposizione1 = 1;
-            var button = this.getView().byId("collapseButton_FinePredisposizione1");
-            button.setEnabled(true);
-        },
-        ShowRelevant_FinePredisposizione1: function () {
-            this.FlagCollapseNotRelevant_FinePredisposizione1 = 1;
-            this.CollapseNotRelevan_FinePredisposizione1();
-            var button = this.getView().byId("collapseButton_FinePredisposizione1");
-            button.setEnabled(false);
-        },
-//        CollapseNotRelevant_FinePredisposizione2: function () {
-//            if (this.FlagCollapseNotRelevant_FinePredisposizione2 == 1 && this.View_FinePredisposizione2 !== null) {
-//                var oGlobalBusyDialog = new sap.m.BusyDialog();
-//                oGlobalBusyDialog.open();
-//                this.View_FinePredisposizione2 = this.getView().byId("TreeTable_FinePredisposizione2");
-//                this.total = this.View_FinePredisposizione2._iBindingLength;
-//                var temp;
-//                for (var i = this.total - 1; i >= 0; i--) {
-//                    temp = this.View_FinePredisposizione2.getContextByIndex(i).getObject();
-//                    if (temp.expand == 0) {
-//                        this.View_FinePredisposizione2.collapse(i);
-//                    }
-//                }
-//                oGlobalBusyDialog.close();
-//                this.FlagCollapseNotRelevant_FinePredisposizione2 = 0;
-//            }
-//        },
-        CollapseAll_FinePredisposizione2: function () {
-            this.View_FinePredisposizione2 = this.getView().byId("TreeTable_FinePredisposizione2");
-            this.View_FinePredisposizione2.collapseAll();
-            this.AllExpanded_FinePredisposizione2 = 0;
-            var button = this.getView().byId("collapseButton_FinePredisposizione2");
-            button.setEnabled(false);
-        },
-        ExpandAll_FinePredisposizione2: function () {
-            this.View_FinePredisposizione2 = this.getView().byId("TreeTable_FinePredisposizione2");
-            this.View_FinePredisposizione2.expandToLevel(100);
-            this.AllExpanded_FinePredisposizione2 = 1;
-            var button = this.getView().byId("collapseButton_FinePredisposizione2");
-            button.setEnabled(true);
-        },
-        ShowRelevant_FinePredisposizione2: function () {
-            this.FlagCollapseNotRelevant_FinePredisposizione2 = 1;
-            this.CollapseNotRelevan_FinePredisposizione2();
-            var button = this.getView().byId("collapseButton_FinePredisposizione2");
-            button.setEnabled(false);
-        },
+
         onNavBack: function () {
             var oHistory = History.getInstance();
             var sPreviousHash = oHistory.getPreviousHash();
@@ -222,29 +101,6 @@ sap.ui.define([
 //                        });
 //            }
 //        },
-
-        // fare expand e basta nell'after rendering e provare a vedere se funzia
-//        onClick: function () {
-//            this.PresaInCarico();
-//            $(window.document).ready(this.onMiao());
-////            this.getOwnerComponent().getRouter().navTo("PresaInCarico", {}, true);
-//
-//        },
-
-        //        onExpandFirstLevel: function () {
-//            var View_PresaInCarico = this.getView().byId("TreeTable_PresaInCarico");
-//            View_PresaInCarico.expandToLevel(100);
-//            if (sap.ui.getCore().byId("PresaInCarico") !== null) {
-//
-//                sap.ui.getCore().byId("PresaInCarico").destroy();
-//
-//            } else {
-//                this.getSplitAppObj().toDetail(this.createId("PresaInCarico"));
-//            }
-//            this.onMiao();
-////            this.bool_expanded = true;
-//        },
-
 
     });
     return TmpController;
