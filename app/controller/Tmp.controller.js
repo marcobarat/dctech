8923ae513684dc7a92ebefa02e3c34643b596083
unknown <battilana.matteo@gmail.com>
2018-04-12 17:59:55 +0200

    aggiunta della pagina modifica condizioni (completa) e della pagina progress bar (incompleta)

--- app/controller/Tmp.controller.js
+++ app/controller/Tmp.controller.js
@@ -9,15 +9,78 @@ sap.ui.define([
     var TmpController = Controller.extend("myapp.controller.Tmp", {
 
         View: null,
+        ModelBatchOld: null,
+        ModelBatchNew: null,
+        ModelSetup: null,
         total: null,
         oGlobalBusyDialog: new sap.m.BusyDialog(),
 
         onInit: function () {
             this.getSplitAppObj().toDetail(this.createId("Home"));
+
+            this.ModelBatchOld = new JSONModel({});
+            this.getView().setModel(this.ModelBatchOld, "ModelBatchOld");
+            var param = {};
+            var req = jQuery.ajax({
+                url: "model/SKU.json",
+                data: param,
+                method: "GET",
+                dataType: "json",
+                async: true,
+                Selected: true
+            });
+            var tempfunc = jQuery.proxy(this.FillModelBatchOld, this);
+            req.done(tempfunc);
+
+            this.ModelBatchNew = new JSONModel({});
+            this.getView().setModel(this.ModelBatchNew, "ModelBatchNew");
+            param = {};
+            req = jQuery.ajax({
+                url: "model/SKU_1.json",
+                data: param,
+                method: "GET",
+                dataType: "json",
+                async: true,
+                Selected: true
+            });
+            tempfunc = jQuery.proxy(this.FillModelBatchNew, this);
+            req.done(tempfunc);
+
+            this.ModelSetup = new JSONModel({});
+            this.getView().setModel(this.ModelSetup, "ModelSetup");
+            param = {};
+            req = jQuery.ajax({
+                url: "model/allestimento.json",
+                data: param,
+                method: "GET",
+                dataType: "json",
+                async: true,
+                Selected: true
+            });
+            tempfunc = jQuery.proxy(this.FillModelSetup, this);
+            req.done(tempfunc);
+
+
+//            DA CANCELLARE
+//            this.getView().byId("ButtonFinePredisposizione").setEnabled(false);
+            this.getView().byId("ButtonModificaCondizioni").setEnabled(true);
+            this.getView().byId("ButtonFermo").setEnabled(true);
+            this.getView().byId("ButtonRiavvio").setEnabled(true);
+            this.getView().byId("ButtonCausalizzazione").setEnabled(true);
+            this.getView().byId("ButtonChiusuraConfezionamento").setEnabled(true);
         },
         PresaInCarico: function () {
             this.getSplitAppObj().toDetail(this.createId("PresaInCarico"));
-            this.FillTreeTable("model/SKU.json", "TreeTable_PresaInCarico");
+            this.FillTreeTable(this.ModelBatchNew, "TreeTable_PresaInCarico");
+        },
+        FillModelBatchOld: function (data) {
+            this.ModelBatchOld.setProperty("/", data);
+        },
+        FillModelBatchNew: function (data) {
+            this.ModelBatchNew.setProperty("/", data);
+        },
+        FillModelSetup: function (data) {
+            this.ModelSetup.setProperty("/", data);
         },
         CollapseNotRelevant: function (TreeName) {
 
@@ -51,8 +114,8 @@ sap.ui.define([
         },
         ConfermaBatch: function () {
             this.getSplitAppObj().toDetail(this.createId("ConfermaBatch"));
-            this.FillTreeTable("model/SKU.json", "TreeTable_ConfermaBatchOld");
-            this.FillTreeTable("model/SKU_1.json", "TreeTable_ConfermaBatchNew");
+            this.FillTreeTable(this.ModelBatchNew, "TreeTable_ConfermaBatchOld");
+            this.FillTreeTable(this.ModelBatchOld, "TreeTable_ConfermaBatchNew");
 //            document.getElementById('panel_processi').classList.add('stylePanelYellow');
             this.getView().byId("panel_processi").addStyleClass("stylePanelYellow");
             this.getView().byId("ButtonPresaInCarico").setEnabled(false);
@@ -68,19 +131,28 @@ sap.ui.define([
 
         FinePredisposizione: function () {
             this.getSplitAppObj().toDetail(this.createId("FinePredisposizione"));
-            this.FillTreeTable("model/allestimento.json", "TreeTable_FinePredisposizione");
-
+            this.FillTreeTable(this.ModelSetup, "TreeTable_FinePredisposizione");
         },
 
         ConfermaPredisposizione: function () {
             this.getSplitAppObj().toDetail(this.createId("InProgress"));
-            this.getView().byId("panel_processi").addStyleClass("stylePanelGreen"); 
-            this.getView().byId("ButtonFinePredisposizione").setEnabled(false);
-            this.getView().byId("ButtonModificaCondizioni").setEnabled(true);
-            this.getView().byId("ButtonFermo").setEnabled(true);
-            this.getView().byId("ButtonRiavvio").setEnabled(true);
-            this.getView().byId("ButtonCausalizzazione").setEnabled(true);
-            this.getView().byId("ButtonChiusuraConfezionamento").setEnabled(true);
+            this.getView().byId("panel_processi").addStyleClass("stylePanelGreen");
+//            this.getView().byId("ButtonFinePredisposizione").setEnabled(false);
+//            this.getView().byId("ButtonModificaCondizioni").setEnabled(true);
+//            this.getView().byId("ButtonFermo").setEnabled(true);
+//            this.getView().byId("ButtonRiavvio").setEnabled(true);
+//            this.getView().byId("ButtonCausalizzazione").setEnabled(true);
+//            this.getView().byId("ButtonChiusuraConfezionamento").setEnabled(true);
+        },
+
+        ModificaCondizioni: function () {
+            this.ModelSetup = this.getView().getModel("ModelSetup");
+            this.getSplitAppObj().toDetail(this.createId("ModificaCondizioni"));
+            this.FillTreeTable(this.ModelSetup, "TreeTable_ModificaCondizioni");
+        },
+
+        ConfermaModifica: function () {
+            this.getSplitAppObj().toDetail(this.createId("InProgress"));
         },
 
         Expander: function (name) {
@@ -88,7 +160,7 @@ sap.ui.define([
             this.View.expandToLevel(100);
         },
         FillTreeTable: function (model, TreeName) {
-            this.getView().setModel(new JSONModel(model), TreeName);
+            this.getView().setModel(model, TreeName);
             this.View = this.getView().byId(TreeName);
             this.oGlobalBusyDialog.open();
 
